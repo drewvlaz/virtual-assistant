db54cef5269437d116e02b8140c36b16a92ed016
drewvlaz <314dsv@gmail.com>
2019-11-17 17:18:58 +0000

    Removed dependencies

--- legacy/dependencies/jsoncpp/include/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/drewvlaz/Downloads/jsoncpp-1.9.1/include
-# Build directory: /home/drewvlaz/Downloads/jsoncpp-1.9.1/include
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- legacy/dependencies/jsoncpp/src/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/drewvlaz/Downloads/jsoncpp-1.9.1/src
-# Build directory: /home/drewvlaz/Downloads/jsoncpp-1.9.1/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("lib_json")
-subdirs("jsontestrunner")
-subdirs("test_lib_json")
