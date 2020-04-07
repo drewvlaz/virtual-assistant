832479458f716840fd49bfd6b1b6aa235240f042
drewvlaz <314dsv@gmail.com>
2020-04-07 19:13:07 -0400

    Add adjacent bubble capability

--- src/main/java/assistant/Bubble.java
+++ src/main/java/assistant/Bubble.java
@@ -6,6 +6,9 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.shape.Rectangle;
 import javafx.geometry.Pos;
+
+import javax.swing.text.AttributeSet.FontAttribute;
+
 import javafx.geometry.Insets;
 import javafx.scene.text.Text;
 import javafx.scene.text.Font;
@@ -16,6 +19,7 @@ public class Bubble extends HBox {
     private Text content;
     private Rectangle background;
     private StackPane bubble;
+    private final int FONT_SIZE = 14;
 
     // Constructors
     public Bubble() {
@@ -42,8 +46,8 @@ public class Bubble extends HBox {
 
     // Style the bubble appropriately
     public void configure(boolean user) {
-        // Set colors and text font, css doesn't work well for this
-        content.setFont(new Font("Arial", 14));
+        // Set colors and text font, css right work well for this
+        content.setFont(new Font("Arial", FONT_SIZE));
         content.setFill(Paint.valueOf(user ? "#ffffff" : "#000000"));
 
         // Set bubble background shape and color
@@ -54,8 +58,8 @@ public class Bubble extends HBox {
         content.setWrappingWidth(Math.min(content.getBoundsInLocal().getWidth(), (500 * 2 / 3.0)));
 
         // Set bubble background width/height and add padding
-        background.setWidth(content.getBoundsInLocal().getWidth() + 14);
-        background.setHeight(content.getBoundsInLocal().getHeight() + 14);
+        background.setWidth(content.getBoundsInLocal().getWidth() + FONT_SIZE);
+        background.setHeight(content.getBoundsInLocal().getHeight() + FONT_SIZE);
 
         // Combine content and background into bubble
         bubble.getChildren().addAll(background, content);
@@ -64,5 +68,11 @@ public class Bubble extends HBox {
         this.getChildren().add(bubble);
         this.setAlignment(user ? Pos.BASELINE_RIGHT : Pos.BASELINE_LEFT);
         this.setPadding(new Insets(10, 10, 10, 10));;
+    }
+
+    // Adjusts the padding so that the bubble is adjacent
+    public void setAdjacent(boolean first, boolean last) {
+        // Insets(top, right, bottom, left)
+        this.setPadding(new Insets(first ? 10 : 0, 10, last ? 10 : 0, 10));;
     }
 }
